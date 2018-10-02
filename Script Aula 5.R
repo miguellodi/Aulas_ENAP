807979a15d97cdf7d7be80c5380a5739a8456dac
miguellodi <miguellodi@gmail.com>
2018-10-01 21:56:25 -0300

    Exercício 2

--- Script Aula 5.R
+++ Script Aula 5.R
@@ -18,3 +18,38 @@ juizes_drogas_CL <- decisoes %>%
 
 write_rds(juizes_drogas_CL, "C:/Users/ML/Documents/Aulas_ENAP/juizes_drogas_CL.rds")
 
+
+processo <- read_rds("C:/Users/ML/Documents/Aulas_ENAP/Dados/processos_nested.rds")
+
+
+dec_gather <- decisoes %>%
+  filter(!is.na(id_decisao)) %>%
+  select(id_decisao:data_registro) %>%
+  gather(key="variavel", value="valor",-id_decisao) %>%
+  arrange(id_decisao)
+
+
+levels(factor(dec_gather$variavel))
+
+decisoes_spread <- decisoes%>%
+  filter(!is.na(id_decisao))%>%
+  select(id_decisao:data_registro)%>% 
+  gather(key, value,-id_decisao) %>%
+  spread(key, value)
+
+
+juizes_drogasL <- decisoes %>%
+  filter(!is.na(txt_decisao)) %>%
+  mutate(txt_decisao = tolower(txt_decisao),
+         droga = str_detect(txt_decisao,
+                            "droga|entorpecente|psicotr[óo]pico|maconha|haxixe|coca[ií]na"),
+         droga=case_when(
+           droga==TRUE ~ "droga",
+           droga==FALSE ~"n_droga" )) %>%
+         group_by(juiz,droga) %>%
+summarise(n=n()) %>%
+spread(droga,n,fill = 0) %>%
+mutate(total=droga+n_droga,
+       proporcao=droga/total) %>%
+  arrange(-proporcao)
+      
